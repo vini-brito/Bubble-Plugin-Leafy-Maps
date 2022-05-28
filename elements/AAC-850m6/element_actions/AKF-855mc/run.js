7edd17f9c99b24c2bd26ec88eb58c51a8532556d
vini-brito <vini_brito@outlook.com>
2022-05-27 22:20:47 -0400

    circle test one

--- elements/AAC-850m6/element_actions/AKF-855mc/run.js
+++ elements/AAC-850m6/element_actions/AKF-855mc/run.js
@@ -1,11 +1,34 @@
 function(instance, properties, context) {
 
 
-  //Load any data 
+  let optionsObject = {
+    stroke: properties.draw_stroke, // use stroke or not
+    color: properties.stroke_color, // stroke color
+    weight: properties.stroke_weight, // stroke width in pixels, default is 3
+    opacity: properties.stroke_opacity, // default is 1.0
+    fill: properties.circle_fill, // enable or disable filling
+    fillColor: properties.fill_color, // color string
+    fillOpacity: properties.fill_opacity, // default is 0.2
+    radius: properties.radius, // number, in meters
+  }
+
+  instance.data[`${properties.circle_name}`] = L.circle(instance.data.mymap.getCenter(), optionsObject).addTo(instance.data.mymap);
 
 
+  // zoom the map to the polygon
 
-  //Do the operation
+  if (properties.zoom_to_this_circle) {
+
+    instance.data.mymap.fitBounds(instance.data[`${properties.circle_name}`].getBounds());
+
+  }
+
+
+instance.data.mymap.on('move', function(e) {
+
+  instance.data[`${properties.circle_name}`].setLatLng(instance.data.mymap.getCenter());
+
+});
 
 
 
