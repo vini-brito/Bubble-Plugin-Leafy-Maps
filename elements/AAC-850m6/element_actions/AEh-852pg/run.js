a0447f844bd1b8b0fefc0bde938217f4ae3ef4c5
Vinicius Brito <6878457+vini-brito@users.noreply.github.com>
2022-08-06 01:32:49 -0400

    Automatic commit from Bubble

--- elements/AAC-850m6/element_actions/AEh-852pg/run.js
+++ elements/AAC-850m6/element_actions/AEh-852pg/run.js
@@ -6,33 +6,34 @@ function(instance, properties, context) {
 
     };
 
-    let instanceKeys = Object.keys(instance.data);
-
-    let foundMarkers = instanceKeys.filter(markerIdentifier);
-
-
-    let markersCount = foundMarkers.length;
-
-    for (i = 0; i < markersCount; i++) {
-
-        if (
-            typeof instance.data[`markerCluster${properties.unique_name_to_erase}${i + 1}`] !== "undefined" &&
-            instance.data[`markerCluster${properties.unique_name_to_erase}${i + 1}`] !== null
-        ) {
-
-            instance.data[`${properties.unique_name_to_erase}${i + 1}`].remove()
-
-        }
-
-    }
-
-
     if (
         typeof instance.data[`markerCluster${properties.unique_name_to_erase}`] !== "undefined" &&
         instance.data[`markerCluster${properties.unique_name_to_erase}`] !== null
     ) {
 
         instance.data[`markerCluster${properties.unique_name_to_erase}`].remove()
+        
+        delete instance.data[`markerCluster${properties.unique_name_to_erase}`]
 
     }
+
+    let instanceKeys = Object.keys(instance.data);
+
+
+    let foundMarkers = instanceKeys.filter(markerIdentifier);
+
+
+    let markersCount = foundMarkers.length;
+
+
+    console.log(foundMarkers)
+
+    for (i = 0; i < markersCount; i++) {
+
+        instance.data[`${properties.unique_name_to_erase}${i + 1}`].remove()
+
+    }
+
+
+
 }
