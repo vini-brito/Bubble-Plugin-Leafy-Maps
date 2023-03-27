6d81a607d055ea5eb14deeffa19ffe1319aa6e9f
Vinicius Brito <6878457+vini-brito@users.noreply.github.com>
2023-03-27 15:46:42 -0400

    Automatic commit from Bubble

--- elements/AAC-850m6/element_actions/ADM-8524u/run.js
+++ elements/AAC-850m6/element_actions/ADM-8524u/run.js
@@ -1,6 +1,12 @@
 function(instance, properties, context) {
 
-instance.data.mymap.flyTo([properties.latitude, properties.longitude], properties.zoom)
+let zoomPanOptions = {
 
+    duration: properties.duration,
+    easeLinearity: properties.easeLinearity,
+
+};
+
+instance.data.mymap.flyTo([properties.latitude, properties.longitude], properties.zoom, zoomPanOptions)
 
 }
